0c232b4b3fa5863f06c70177f1bf0a5ae096b593
nooryeaserkhan <nooryeaserkhan@192.168.1.107>
2019-11-25 21:11:02 +0600

    Added Grid Configuration & Parallel Test settings.

--- OrangeHRM/src/test/java/helper/Base.java
+++ OrangeHRM/src/test/java/helper/Base.java
@@ -1,22 +1,47 @@
 package helper;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Base {
 	
 public WebDriver driver;
-	
+	@Parameters({"Port"})
 	@BeforeClass
-	public void StartBrowser(){			
-		WebDriverManager.chromedriver().setup();
+	public void StartBrowser(String Port) throws MalformedURLException{			
+		/*WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();			
 		driver.manage().window().maximize();
-		driver.get("https://opensource-demo.orangehrmlive.com/index.php/auth/login");
+		driver.get("https://opensource-demo.orangehrmlive.com/index.php/auth/login");*/
+		
+		if(Port.equalsIgnoreCase("27621")) {
+			ChromeOptions options = new ChromeOptions();		
+			String hubUrl="http://localhost:4444/wd/hub";
+			this.driver = new RemoteWebDriver(new URL(hubUrl), options);
+			driver.manage().window().maximize();
+			driver.get("https://opensource-demo.orangehrmlive.com/index.php/auth/login");
+		}
+		else {
+			FirefoxOptions options = new FirefoxOptions();		
+			String hubUrl="http://localhost:4444/wd/hub";
+			this.driver = new RemoteWebDriver(new URL(hubUrl), options);
+			driver.manage().window().maximize();
+			driver.get("https://opensource-demo.orangehrmlive.com/index.php/auth/login");
+		}	
+		
 	  }
 	
 	@AfterClass
